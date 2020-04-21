a0477669f5831415136df3d8336e4ad94536333d
Lupu <5337610+lupu60@users.noreply.github.com>
2020-04-21 16:39:24 +0200

    update upsert
    * add option to upsert array of objects
    * add the capability to use doNotUpsert without keyNamingTransform
    * change snake-case to camel case

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
-import _ from 'lodash'
+import * as _ from 'lodash';
+import { Repository } from 'typeorm';
 
 /*
- * EntityType - TypeORM Entity
- * obj - Object to upsert
- * key_naming_transform (optional) - Transformation to apply to key names before upsert
- * do_not_upsert - Keys to exclude from upsert. This is useful if a non-nullable field is required in case
+ * ⚠️ PostgreSQL compatible only ⚠️
+ * Repository - TypeORM Repository
+ * object - Object to upsert or an array of objects
+ * keyNamingTransform (optional) - Transformation to apply to key names before upsert
+ * doNotUpsert - Keys to exclude from upsert. This is useful if a non-nullable field is required in case
  * the row does not already exist but you do not want to overwrite this field if it already exists
  */
-export default async <T>(
-    EntityType: T,
-    obj: T,
-    primary_key: string,
-    opts?: {
-        key_naming_transform: (k: string) => string,
-        do_not_upsert: string[],
-    }
-): Promise<T> => {
-    const keys: string[] = _.difference(_.keys(obj), opts ? opts.do_not_upsert : [])
-    const setter_string =
-        keys.map(k => `${opts ? opts.key_naming_transform(k) : k} = :${k}`)
-
-    const qb = (EntityType as any).createQueryBuilder()
-        .insert()
-        .values(obj)
-        .onConflict(`("${primary_key}") DO UPDATE SET ${setter_string}`)
-
-    keys.forEach(k => {
-        qb.setParameter(k, (obj as any)[k])
-    })
-
-    return (await qb.returning('*').execute()).generatedMaps[0] as T;
+export async function TypeOrmUpsert<T>(
+    repository: Repository<T>,
+    object: T | T[],
+    conflictKey: string,
+    options?: {
+        keyNamingTransform?: (k: string) => string;
+        doNotUpsert?: string[];
+    },
+): Promise<T[]> {
+    options.keyNamingTransform = options.keyNamingTransform || ((k) => k);
+    const keys: string[] = _.difference(_.keys(_.isArray(object) ? object[0] : object), options.doNotUpsert || []);
+    const setterString = keys.map((k) => `${options.keyNamingTransform(k)} = excluded.${k}`);
+    const onConflict = `("${conflictKey}") DO UPDATE SET ${setterString.join(',')}`;
+    const qb = repository.createQueryBuilder().insert().values(object).onConflict(onConflict);
+    return (await qb.returning('*').execute()).raw;
 }
